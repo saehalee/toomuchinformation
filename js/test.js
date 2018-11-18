5fb57e58bef550acbea99236516b0d9b6e490ae4
jeongheetaek <chung1876@naver.com>
2018-11-18 13:53:31 +0900

    inital commit

---
+++ js/test.js
@@ -0,0 +1,9 @@
+// find elements
+var banner = $("#banner-message")
+var btn = $("uploadFile2")
+
+// handle click and add class
+button.on("click", function() {
+  banner.addClass("alt")
+})
+
---
+++ useless/fword.php
@@ -0,0 +1,2 @@
+<a href='http://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=맘스터치&amp;aq=0&amp;sll=37.0625,-95.677068&amp;sspn=61.282355,146.513672&amp;ie=UTF8&amp;hq=&amp;hnear=맘스터치&amp;ll=40.649974,-73.950005&amp;spn=0.01628,0.025663&amp;z=14&amp;iwloc=A&amp;output=embed'>위치확인하기</a>
+
